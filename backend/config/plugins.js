56a3a054096e869666f9255e6535e3970f732ada
Alireza <Aalirezabs@gmail.com>
2023-03-30 15:27:39 +0330

    minor change

--- backend/config/plugins.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({ env }) => ({
-  "users-permissions": {
-    jwtSecret: env("JWT_SECRET") || pass,
-    jwtExpiresIn: 7 * 24 * 60 * 60, // JWT expiration time set to 1 hour
-  },
-});
